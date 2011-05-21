20115262512d2c4f572715f6b9cc1253fabb6692
Gary Capell <gary.capell@gmail.com>
2011-05-21 13:27:58 +1000

    Solves sample input

--- FileFixit/FileFixit.go
+++ FileFixit/FileFixit.go
@@ -5,20 +5,50 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"codejam/ProblemReader"
 )
+
+type Tree map[string]Tree
+
+func newTree() Tree {
+	return Tree(make(map[string]Tree))
+}
+
+/* Add path to tree, return number of components added */
+func (tree Tree) add(path string) int {
+	// fmt.Println("adding ", path, "to", tree)
+	components := strings.Split(path[1:], "/", -1)
+	additions := 0
+	pos := tree
+	for _, c := range components {
+		child, ok := pos[c]
+		if !ok {
+			child = newTree()
+			pos[c] = child
+			additions++
+		}
+		pos = child
+	}
+	// fmt.Printf("add %s -> %#v : %d\n", path, components, additions)
+	return additions
+}
 
 func solver(in *ProblemReader.ProblemReader) string {
 	nums := in.Nums(2)
 	existing, toCreate := nums[0], nums[1]
-	for j :=0; j<existing; j++ {
-		fmt.Println("existing:", in.Line())
+
+	tree := newTree()
+
+	for j := 0; j < existing; j++ {
+		tree.add(in.Line())
 	}
-	for j :=0; j<toCreate; j++ {
-		fmt.Println("toCreate:", in.Line())
+	additions := 0
+	for j := 0; j < toCreate; j++ {
+		additions += tree.add(in.Line())
 	}
 
-	return fmt.Sprintf("Dude")
+	return fmt.Sprintf("%d", additions)
 }
 
 func main() {
