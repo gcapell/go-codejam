dc99e254bed54a2260d71be925ee62f6dc135d37
Gary Capell <gary.capell@gmail.com>
2011-05-21 12:44:02 +1000

    Solves sample

--- RopeIntranet/RopeIntranet.go
+++ RopeIntranet/RopeIntranet.go
@@ -5,16 +5,48 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"codejam/ProblemReader"
 )
 
+type Wire struct {
+	a,b int
+}
+
+type WireSlice [] Wire
+
+func (ws WireSlice) Len() int {
+	return len(ws)
+}
+
+func (ws WireSlice) Less(i,j int) bool {
+	return ws[i].a < ws[j].a
+}
+
+func (ws WireSlice) Swap(i,j int)  {
+	ws[i], ws[j] = ws[j], ws[i]
+}
+
+
 func solver(in *ProblemReader.ProblemReader)(string) {
 	n := in.Num()
+	wires := make([]Wire, n)
+
 	for j:=0; j<n; j++ {
 		wire := in.Nums(2)
-		fmt.Println("wire", j, wire)
+		wires[j] = Wire{wire[0], wire[1]}
 	}
-	return "dude"
+	sort.Sort(WireSlice(wires))
+
+	cross := 0
+	for j := 0; j<n; j++ {
+		for k := j+1; k<n; k++ {
+			if wires[k].b < wires[j].b {
+				cross++
+			}
+		}
+	}
+	return fmt.Sprintf("%d", cross)
 }
 
 func main() {
