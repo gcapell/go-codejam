7d70e05a789fa2c5680e58dd8803b6871e32e1d7
Gary Capell <gary.capell@gmail.com>
2011-05-21 12:19:03 +1000

    Reads sample input

---
+++ RopeIntranet/RopeIntranet.go
@@ -0,0 +1,22 @@
+/* See http://code.google.com/codejam/contest/dashboard?c=32016#s=p0
+ */
+
+package main
+
+import (
+	"fmt"
+	"codejam/ProblemReader"
+)
+
+func solver(in *ProblemReader.ProblemReader)(string) {
+	n := in.Num()
+	for j:=0; j<n; j++ {
+		wire := in.Nums(2)
+		fmt.Println("wire", j, wire)
+	}
+	return "dude"
+}
+
+func main() {
+	ProblemReader.In.SolveProblems(solver)
+}
