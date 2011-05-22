deb2a8a60b54244d3463d00603c065c93c848dd6
Gary Capell <gary.capell@gmail.com>
2011-05-22 21:43:24 +1000

    Broken implementation of checking for winners

--- Rotate/Rotate.go
+++ Rotate/Rotate.go
@@ -11,11 +11,90 @@ import (
 func solver(in *ProblemReader.ProblemReader) string {
 	nums := in.Nums(2)
 	board, toWin := nums[0], nums[1]
-	fmt.Println("board:", board, "toWin:", toWin)
+	lines := make([] string, board)
 	for j := 0; j < board; j++ {
-		fmt.Println("line", in.Line())
+		line := []byte(in.Line())
+		shiftLine(line)
+		lines[j] = string(line)
 	}
-	return "Dude!"
+	return winner(lines, board, toWin)
+}
+
+func winner(lines []string, board int, toWin int) (reply string) {
+	fmt.Println("board:", board, "toWin:", toWin)
+	for row, line := range lines {
+		fmt.Println(line, row)
+	}
+	redWin := findWin(lines, board, toWin, 'R')
+	blueWin := findWin(lines, board, toWin, 'B')
+	
+	switch {
+	case redWin && blueWin: reply = "Both"
+	case redWin: reply = "Red"
+	case blueWin: reply = "Blue"
+	default: reply = "Neither"
+	}
+	return reply
+}
+
+func findWin(lines []string, board int, toWin int, c byte) bool {
+	fmt.Println("findWin", string(c))
+	
+	for row :=len(lines)-1; row >=0; row-- {
+		first := len(lines)-toWin
+		found := 0
+
+		for first >=0 && lines[row][first]!='.' {
+			required := toWin - found
+			found = 0
+			for pos := first; pos < first + required; pos++ {
+				fmt.Println(row, pos, string(lines[row][pos]))
+				if lines[row][pos] == c {
+					found++
+				} else {
+					break
+				}
+			}
+			if found == required {
+				fmt.Println("success!")
+				return true
+			}
+		}
+	}
+
+	// Vertical
+	fmt.Println("vertical")
+	for col :=len(lines)-1; col >=0; col-- {
+		first := len(lines)-toWin
+
+		vertical:
+		for first >=0 && lines[first][col]!='.' {
+			for pos := first; pos < first + toWin; pos++ {
+				fmt.Println(pos, col, string(lines[pos][col]))
+				if lines[pos][col] != c {
+					first = pos - toWin
+					continue vertical
+				}
+			}
+			fmt.Println("success!")
+			return true
+		}
+	}
+	
+	return false
+}
+
+func shiftLine(line []byte)  {
+	writePos := len(line)-1
+	for readPos := len(line)-1; readPos >=0; readPos-- {
+		if line[readPos] != '.' {
+			line[writePos] = line[readPos]
+			writePos--
+		}
+	}
+	for ;writePos >= 0; writePos-- {
+		line[writePos] = '.'
+	}
 }
 
 func main() {
