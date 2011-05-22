967619911ee33937ad3183c04590528569de405d
Gary Capell <gary.capell@gmail.com>
2011-05-23 08:38:40 +1000

    gofmt

--- Rotate/Rotate.go
+++ Rotate/Rotate.go
@@ -17,8 +17,8 @@ func assert(b bool) {
 func solver(in *ProblemReader.ProblemReader) string {
 	nums := in.Nums(2)
 	board, toWin := nums[0], nums[1]
-	assert (toWin <= board)
-	lines := make([] string, board)
+	assert(toWin <= board)
+	lines := make([]string, board)
 	for j := 0; j < board; j++ {
 		line := []byte(in.Line())
 		assert(len(line) == board)
@@ -28,16 +28,16 @@ func solver(in *ProblemReader.ProblemReader) string {
 	return winner(lines, toWin)
 }
 
-func shiftLine(line []byte)  {
-	writePos := len(line)-1
-	for readPos := len(line)-1; readPos >=0; readPos-- {
+func shiftLine(line []byte) {
+	writePos := len(line) - 1
+	for readPos := len(line) - 1; readPos >= 0; readPos-- {
 		if line[readPos] != '.' {
-			assert (line[readPos] == 'R' || line[readPos] == 'B')
+			assert(line[readPos] == 'R' || line[readPos] == 'B')
 			line[writePos] = line[readPos]
 			writePos--
 		}
 	}
-	for ;writePos >= 0; writePos-- {
+	for ; writePos >= 0; writePos-- {
 		line[writePos] = '.'
 	}
 }
@@ -50,19 +50,23 @@ func winner(lines []string, toWin int) (reply string) {
 	}
 	redWin := findWin(lines, toWin, 'R')
 	blueWin := findWin(lines, toWin, 'B')
-	
+
 	switch {
-	case redWin && blueWin: reply = "Both"
-	case redWin: reply = "Red"
-	case blueWin: reply = "Blue"
-	default: reply = "Neither"
+	case redWin && blueWin:
+		reply = "Both"
+	case redWin:
+		reply = "Red"
+	case blueWin:
+		reply = "Blue"
+	default:
+		reply = "Neither"
 	}
 	return reply
 }
 
 func findWin(lines []string, toWin int, c byte) bool {
 	//fmt.Println("findWin", string(c))
-	
+
 	board := len(lines)
 	sequenceChan := make(chan sequence)
 	go listSequences(board, sequenceChan)
@@ -90,16 +94,16 @@ type point struct {
 }
 
 type sequence struct {
-	name string
+	name  string
 	start point
-	next func(point) point
+	next  func(point) point
 }
 
-func(s sequence) String() string {
+func (s sequence) String() string {
 	return fmt.Sprintf("%s %v", s.name, s.start)
 }
 
-func(p point) onBoard(board int) bool {
+func (p point) onBoard(board int) bool {
 	return p.row >= 0 && p.row < board && p.col >= 0 && p.col < board
 }
 
@@ -108,16 +112,16 @@ func listSequences(board int, c chan sequence) {
 	next := func(p point) point {
 		return point{p.row, p.col + 1}
 	}
-	for j :=0; j<board; j++ {
-		c <- sequence{ "right", point{j,0}, next}
+	for j := 0; j < board; j++ {
+		c <- sequence{"right", point{j, 0}, next}
 	}
 
 	// Down
 	next = func(p point) point {
-		return point{p.row + 1, p.col }
+		return point{p.row + 1, p.col}
 	}
-	for j :=0; j<board; j++ {
-		c <- sequence{ "down", point{0, j}, next}
+	for j := 0; j < board; j++ {
+		c <- sequence{"down", point{0, j}, next}
 	}
 
 	// Bottom left to top right
@@ -125,23 +129,23 @@ func listSequences(board int, c chan sequence) {
 		return point{p.row - 1, p.col + 1}
 	}
 
-	for j :=0; j<board; j++ {
-		c <- sequence{ "updiag", point{board -1 -j, 0}, next}
-		c <- sequence{ "updiag", point{ board-1, j}, next}
+	for j := 0; j < board; j++ {
+		c <- sequence{"updiag", point{board - 1 - j, 0}, next}
+		c <- sequence{"updiag", point{board - 1, j}, next}
 	}
 
 	// Top left to bottom right
 	next = func(p point) point {
-		return point{p.row+1, p.col + 1}
+		return point{p.row + 1, p.col + 1}
 	}
 
-	for j :=0; j<board; j++ {
-		c <- sequence{ "downdiag", point{0, j}, next}
-		c <- sequence{ "downdiag", point{j, 0}, next}
+	for j := 0; j < board; j++ {
+		c <- sequence{"downdiag", point{0, j}, next}
+		c <- sequence{"downdiag", point{j, 0}, next}
 	}
 	close(c)
 }
-	
+
 func main() {
 	ProblemReader.In.SolveProblems(solver)
 }
