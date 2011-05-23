1330696958f5f599a67869268c5a85739bfe9412
Gary Capell <ook@google.com>
2011-05-23 13:40:13 +1000

    Set up environment at work

---
+++ env.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+GOROOT=/usr/local/google/users/ook/pkg/go
+PATH=$GOROOT/bin:/home/ook/bin:/usr/local/symlinks:/usr/local/scripts:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/ook/pkg/plan9/bin
