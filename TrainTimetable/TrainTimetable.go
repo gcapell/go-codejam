ee4c31fa3b9e91db3100a2fcd48c4019eb3a7827
Gary Capell <ook@google.com>
2011-05-23 18:59:40 +1000

    gofmt

--- TrainTimetable/TrainTimetable.go
+++ TrainTimetable/TrainTimetable.go
@@ -14,12 +14,12 @@ import (
 )
 
 const (
-	A= "A"
-	B= "B"
+	A = "A"
+	B = "B"
 )
 
 type trip struct {
-	src, dst string
+	src, dst       string
 	depart, arrive int
 }
 
@@ -30,9 +30,9 @@ type myHeap struct {
 func (h *myHeap) Less(i, j int) bool { return h.At(i).(int) < h.At(j).(int) }
 
 type station struct {
-	name string
+	name     string
 	reserved int
-	waiting myHeap
+	waiting  myHeap
 }
 
 func (s *station) clear() {
@@ -41,29 +41,29 @@ func (s *station) clear() {
 }
 
 var (
-	allStations = map[string] *station {A: &station{name:A},B: &station{name:B}}
+	allStations = map[string]*station{A: &station{name: A}, B: &station{name: B}}
 )
 
-type tripArray [] trip
+type tripArray []trip
 
 func solver(in *ProblemReader.ProblemReader) string {
 	turnAround := in.Num()
 	nums := in.Nums(2)
 	nA, nB := nums[0], nums[1]
-	allTrips := make([]trip, nA + nB)
-	
-	for _,v := range allStations {
+	allTrips := make([]trip, nA+nB)
+
+	for _, v := range allStations {
 		v.clear()
 	}
 
-	for j :=0; j< nA; j++ {
+	for j := 0; j < nA; j++ {
 		allTrips[j] = readTrip(in, A, B)
 	}
-	for j :=0; j< nB; j++ {
-		allTrips[nA +j ] = readTrip(in, B, A)
+	for j := 0; j < nB; j++ {
+		allTrips[nA+j] = readTrip(in, B, A)
 	}
 	sort.Sort(tripArray(allTrips))
-	
+
 	for _, t := range allTrips {
 		src, dst := allStations[t.src], allStations[t.dst]
 
@@ -78,7 +78,7 @@ func solver(in *ProblemReader.ProblemReader) string {
 
 func (s *station) getTrain(departure int) {
 	h := &s.waiting
-	if h.Len() > 0  && h.At(0).(int) <= departure {
+	if h.Len() > 0 && h.At(0).(int) <= departure {
 		heap.Pop(h)
 	} else {
 		s.reserved++
@@ -91,7 +91,7 @@ func (s *station) addTrain(arrival int) {
 
 func readTrip(in *ProblemReader.ProblemReader, src string, dst string) trip {
 	sched := in.Words()
-	return trip { src , dst, parseT(sched[0]), parseT(sched[1]) }
+	return trip{src, dst, parseT(sched[0]), parseT(sched[1])}
 }
 
 func parseT(s string) int {
@@ -110,11 +110,10 @@ func (t tripArray) Len() int {
 	return len(t)
 }
 
-func (t tripArray) Less(i,j int) bool {
+func (t tripArray) Less(i, j int) bool {
 	return t[i].depart < t[j].depart
 }
 
-func (t tripArray) Swap(i,j int) {
+func (t tripArray) Swap(i, j int) {
 	t[i], t[j] = t[j], t[i]
 }
-
